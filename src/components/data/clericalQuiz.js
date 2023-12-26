26249648e109731a989524ff0f2fa5c4bc4454fd
Meowmeowzers <95127319+Meowmeowzers@users.noreply.github.com>
2023-12-27 00:37:28 +0800

    Initial clerical data added

--- src/components/data/clericalQuiz.js
+++ src/components/data/clericalQuiz.js
@@ -1,24 +1,112 @@
 const clericalQuizData = [
 	{
 		id: 12001,
-		question: "question c",
-		answer: "answer",
+		question: "This is a type of document issued by a seller to a buyer relating to a sale transaction and indicating the products, quantities and prices sold to the buyer.",
+		answer: "Sales invoice",
 		choices: [
-			"choice 1x",
-			"choice 2y",
-			"choice 3z",
-			"choice 4a"
+			"Check voucher",
+			"Purchase order",
+			"Delivery receipt",
+			"Transcript of Records"
 		]
 	},
 	{
 		id: 12002,
-		question: "question c",
-		answer: "answer",
+		question: "Which of the following is not a usual document used in a sales company?",
+		answer: "Form 137",
 		choices: [
-			"choice 1x",
-			"choice 2y",
-			"choice 3z",
-			"choice 4a"
+			"Check voucher",
+			"Delivery receipt",
+			"Sales invoice",
+			"Purchase order"
+		]
+	},
+	{
+		id: 12003,
+		question: "One of the responsibilities of a clerical job is answering and tending phone calls. Which is the best way to answer a phone call in a company?",
+		answer: "Answer the phone politely and give your department or company name.",
+		choices: [
+			"Say “Hello,” and wait for the caller to speak.",
+			"Run and get a pen and notebook then pick up the phone.",
+			"Get the phone and ask them if they have eaten already.",
+			"Wait for fellow employees to answer the phone."
+		]
+	},
+	{
+		id: 12004,
+		question: "An example of a hard copy document is:",
+		answer: "Business letter in a paper ",
+		choices: [
+			"PDF file letter",
+			"Company email message",
+			"A business letter saved in USB",
+			"The company chat message"
+		]
+	},
+	{
+		id: 12005,
+		question: "What does a CC means in an email message?",
+		answer: "Copy furnished",
+		choices: [
+			"Copy sent",
+			"Crowd control",
+			"Copy cat",
+			"Client copy"
+		]
+	},
+	{
+		id: 12006,
+		question: "Which computer program should you go if you want to email a company?",
+		answer: "Outlook",
+		choices: [
+			"Word",
+			"Excel",
+			"Access",
+			"Powerpoint"
+		]
+	},
+	{
+		id: 12007,
+		question: "This is a telephonic transmission of scanned documents of texts and images to a telephone number connected to a printer.",
+		answer: "Fax Machine",
+		choices: [
+			"Photocopying machine",
+			"Scanner",
+			"Typewriter",
+			"Inkjet Printer"
+		]
+	},
+	{
+		id: 12008,
+		question: "The chief financial officer is responsible for the financial matters and financial management of a corporation, she is also known as the _______.",
+		answer: "Treasurer",
+		choices: [
+			"Auditor",
+			"Secretary",
+			"Chief Executive Officer",
+			"Manager"
+		]
+	},
+	{
+		id: 12009,
+		question: "Which department of a company is responsible for cash register operations and payment processing?",
+		answer: "Cashier",
+		choices: [
+			"Chief Executive Officer",
+			"Billing",
+			"Accounting",
+			"Budget"
+		]
+	},
+	{
+		id: 12010,
+		question: "Your boss asked you to send her a soft copy of your latest résumé. An example of a soft copy is:",
+		answer: "A copy saved in a computer and sent through email",
+		choices: [
+			"A print out copy usually in a paper",
+			"Original copy written in a paper",
+			"A black and white copy sent in person",
+			"A copy from a Xerox machine"
 		]
 	}
 ]

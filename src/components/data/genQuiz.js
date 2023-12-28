9388207710a8a61d5f0959f3fe75d37a5b3ada0e
Meowmeowzers <95127319+Meowmeowzers@users.noreply.github.com>
2023-12-29 07:38:21 +0800

    Added general info questions

--- src/components/data/genQuiz.js
+++ src/components/data/genQuiz.js
@@ -1,27 +1,196 @@
 const generalQuizData = [
 	{
 		id: 13001,
-		question: "question right",
+		question: "The official motto used in 30th Southeast Asian Games is what?",
 		image: "",
-		answer: "answer",
+		answer: "We Win As One",
 		choices: [
-			"choice 1x",
-			"choice 2y",
-			"choice 3z",
-			"choice 4a"
+			"We Care As One",
+			"We Play As One",
+			"We Will As One",
+			"We Work As One"
 		],
 		guide: ""
 	},
 	{
 		id: 13002,
-		question: "question right",
+		question: "On August 16, 2018, the government announced at least ___ Philippine holidays for 2019 as declared by virtue of Proclamation No. 555, series of 2018.",
 		image: "",
-		answer: "answer",
+		answer: "18",
 		choices: [
-			"choice 1x",
-			"choice 2y",
-			"choice 3z",
-			"choice 4a"
+			"16",
+			"19",
+			"15",
+			"14"
+		],
+		guide: ""
+	},
+	{
+		id: 13003,
+		question: "Which of the following statements best describes the relationship between global warming ozone depletion?",
+		image: "",
+		answer: "Ozone depletion contributes to global warming.",
+		choices: [
+			"Global warming causes ozone depletion.",
+			"Ozone depletion and global warming have nothing to do with each other",
+			"Excessive build-up of carbon dioxide causes both global warming and ozone depletion.",
+			"Ozone depletion contributes to build-up of carbon dioxide"
+		],
+		guide: ""
+	},
+	{
+		id: 13004,
+		question: "It is only Philippine province with an English name.",
+		image: "",
+		answer: "Mountain Province",
+		choices: [
+			"Cebu",
+			"General Santos",
+			"Zambales",
+			"Nueva Vizcaya"
+		],
+		guide: ""
+	},
+	{
+		id: 13005,
+		question: "What form of government does the Philippines adopt?",
+		image: "",
+		answer: "Both Republican and Democratic",
+		choices: [
+			"Republican",
+			"Democratic",
+			"Oligarchy",
+			"None of the choices"
+		],
+		guide: ""
+	},
+	{
+		id: 13006,
+		question: " Who among the following may issue a warrant of arrest or a search warrant?",
+		image: "",
+		answer: "A judge",
+		choices: [
+			"A senator",
+			"A congressman",
+			"The cashier",
+			"The President"
+		],
+		guide: ""
+	},
+	{
+		id: 13007,
+		question: "A foreigner may acquire Filipino citizenship through",
+		image: "",
+		answer: "Naturalization",
+		choices: [
+			"Extradition",
+			"Rebirth",
+			"Fixer",
+			"Visa application"
+		],
+		guide: ""
+	},
+	{
+		id: 13008,
+		question: "What is regarded by the State as a \"primary social economic force?\"",
+		image: "",
+		answer: "labor",
+		choices: [
+			"education",
+			"trade",
+			"luxuries",
+			"commerce"
+		],
+		guide: "Under State Policies, the State \"affirms labor as a primary social economic force.\" (Article II, Section 18)"
+	},
+	{
+		id: 13009,
+		question: "According to Article III, Section 15 of the Constitution, the writ of habeas corpus may be suspended in times of rebellion or what?",
+		image: "",
+		answer: "invasion",
+		choices: [
+			"martial law",
+			"war",
+			"terrorism",
+			"crime"
+		],
+		guide: "The writ of habeas corpus prevents persons from	being arrested without warrants of arrest. The Constitution	specifies the conditions wherein the writ shall be suspended to prevent warrantless arrests, which were prevalent during the Marcos dictatorship (which was overthrown by the 1st People Power Revolution). The Constitution clearly states that \"the writ of habeas corpus shall not be suspended except in cases of invasion or rebellion.\""
+	},
+	{
+		id: 13010,
+		question: "The Lower Chamber of the Congress of the Philippines is known by what name?",
+		image: "",
+		answer: "House of Representatives",
+		choices: [
+			"Senate",
+			"Congressional Lower Chamber",
+			"National Assembly",
+			"Barangay Officials"
+		],
+		guide: "Article VI, Section 1 states, \"The legislative power shall be vested in the Congress of the Philippines which shall consist of a Senate and a House of Representatives...\". The Upper House of Congress is the Senate while the Lower House	is the House of Representatives. Representatives are elected by the constituents in their respective legislative districts, while Senators are elected at the national level."
+	},
+	{
+		id: 13011,
+		question: "A Member of either house of Congress may be expelled by their fellow Members. For a Member to be expelled, how much of the total number of Members of a house must concur with the expulsion?",
+		image: "",
+		answer: "two-thirds",
+		choices: [
+			"one half",
+			"majority",
+			"three-fourths",
+			"nine-tenths"
+		],
+		guide: "Article VI, Section 16.3 states, \"Each House may determine the rules of its proceedings, punish its Members for disorderly behavior, and, with the concurrence of twothirds of all its Members, suspend or expel a Member.\" So, if the Senate wants to expel a member, 16 of the 24 Senators must agree to expel that Senator."
+	},
+	{
+		id: 13012,
+		question: "If a President wishes to veto a bill, he/she must communicate it within a certain span of time, otherwise the bill will become a law. How long is this span of time?",
+		image: "",
+		answer: "30 days",
+		choices: [
+			"3 weeks",
+			"48 days",
+			"60 days",
+			"90 days"
+		],
+		guide: "According to Article VI, Section 27.1, \"The President shall communicate his veto of any bill to the House where it originated within thirty days after the date of receipt thereof, otherwise, it shall become a law as if he had signed it.\" A	vetoed bill may still become a law if two-thirds of the members of the house where it originated votes to make it a law."
+	},
+	{
+		id: 13013,
+		question: "What project of the DENR that includes the protection of Industrial pollution of our Environment?",
+		image: "",
+		answer: "Solid Waste Management",
+		choices: [
+			"Bantay Kalikasan",
+			"Bantay Ilog",
+			"Bantay Dagat",
+			"Ecological Watch"
+		],
+		guide: ""
+	},
+	{
+		id: 13014,
+		question: "What government agency thus the status of government employee in the free voluntary services belong?",
+		image: "",
+		answer: "Barangay",
+		choices: [
+			"Secretary",
+			"Regional",
+			"Municipal",
+			"National"
+		],
+		guide: ""
+	},
+	{
+		id: 13015,
+		question: "Which is not a ground for Impeachment of the Pres., Vice-Pres., Ombudsman, and Senator?",
+		image: "",
+		answer: "Against the family if the child is scolded by the parents",
+		choices: [
+			"Graft and corruption",
+			"Sexual harassment",
+			"Grave abuse of the authority",
+			"Other crimes"
 		],
 		guide: ""
 	}

4244690b7cb0467d58b03ad86bf258910a5bed6a
Meowmeowzers <95127319+Meowmeowzers@users.noreply.github.com>
2023-12-30 12:26:04 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/civ-svc-quiz-reviewer/precache-manifest.a6f0aa1bebfbb73caec31d3ab1fd822a.js"
+  "/precache-manifest.c0975694366ba335cc4fbc9f80b4648b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/civ-svc-quiz-reviewer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

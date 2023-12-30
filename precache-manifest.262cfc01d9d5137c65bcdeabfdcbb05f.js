84e4725e44266e5fe242e937ae1226d04f57dba2
Meowmeowzers <95127319+Meowmeowzers@users.noreply.github.com>
2023-12-30 12:03:36 +0800

    Updates

---
+++ precache-manifest.262cfc01d9d5137c65bcdeabfdcbb05f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b18509d8bbb4cbe59eb6bc21045f996",
+    "url": "/civ-svc-quiz-reviewer/index.html"
+  },
+  {
+    "revision": "888e2139059bd5f4d369",
+    "url": "/civ-svc-quiz-reviewer/static/css/main.67849dbf.chunk.css"
+  },
+  {
+    "revision": "fa27f5aecaa506492461",
+    "url": "/civ-svc-quiz-reviewer/static/js/2.761f4138.chunk.js"
+  },
+  {
+    "revision": "2a03a02ae21758236cc96053ed5846ed",
+    "url": "/civ-svc-quiz-reviewer/static/js/2.761f4138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaf5e28c0e532162f3ef",
+    "url": "/civ-svc-quiz-reviewer/static/js/3.5f771c87.chunk.js"
+  },
+  {
+    "revision": "888e2139059bd5f4d369",
+    "url": "/civ-svc-quiz-reviewer/static/js/main.ef52c4de.chunk.js"
+  },
+  {
+    "revision": "a02e4777a75fec496c6b",
+    "url": "/civ-svc-quiz-reviewer/static/js/runtime-main.52069f14.js"
+  }
+]);
